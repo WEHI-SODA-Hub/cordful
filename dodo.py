10f079f17955ca2e5b4d99f49fe2d83ea4798fe7
Michael Milton <michael.r.milton@gmail.com>
2025-01-08 10:41:57 +1100

    Add PCDM generation

--- dodo.py
+++ dodo.py
@@ -2,11 +2,12 @@ from functools import partial
 from pathlib import Path
 from schema_automator.importers.rdfs_import_engine import RdfsImportEngine
 from linkml_runtime.dumpers import YAMLDumper
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 
+models_root = Path(__file__).parent.resolve() / "models"
 
 def rdf_to_linkml(rdf_urls: list[str], subdir: str, format: str) -> None:
-    models_dir = Path(__file__).parent.resolve() / "models" / subdir
+    models_dir = models_root / subdir
     models_dir.mkdir(exist_ok=True)
 
     for model_url in rdf_urls:
@@ -86,4 +87,23 @@ def task_sdo():
             )
         ],
         "targets": ["models/sdo/schemaorg-current-https.yaml"],
+    }
+
+def task_pcdm():
+    """
+    Converts PCDM from RDF to LinkML YAML
+    """
+    # Using my fork of the PCDM ontology due to https://github.com/duraspace/pcdm/issues/80
+    base_url = "https://raw.githubusercontent.com/multimeric/pcdm/refs/heads/fix-80/"
+    urls = ["models.rdf", "pcdm-ext/file-format-types.rdf", "pcdm-ext/rights.rdf", "pcdm-ext/use.rdf", "pcdm-ext/works.rdf"]
+    return {
+        "actions": [
+            partial(
+                rdf_to_linkml,
+                [urljoin(base_url, x) for x in urls],
+                subdir="pcdm",
+                format="xml"
+            )
+        ],
+        "targets": [str(models_root / path) for path in urls],
     }
