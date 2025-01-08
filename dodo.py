f72c9fa12b4e33f4483ec811bfac015658cdb868
Michael Milton <michael.r.milton@gmail.com>
2025-01-08 13:10:02 +1100

    Models page

--- dodo.py
+++ dodo.py
@@ -1,93 +1,97 @@
 from functools import partial
 from pathlib import Path
+from typing import Any, Iterable
 from schema_automator.importers.rdfs_import_engine import RdfsImportEngine
 from linkml_runtime.dumpers import YAMLDumper
 from urllib.parse import urlparse, urljoin
+from linkml.linter.linter import Linter, RuleLevel
 
 models_root = Path(__file__).parent.resolve() / "models"
 
-def rdf_to_linkml(rdf_urls: list[str], subdir: str, format: str) -> None:
+def url_to_path(url: str, subdir: str, suffix: str = ".yml") -> Path:
+    """
+    Converts a URL to a local file path
+
+    Params:
+        url: URL of the remote file
+        subdir: Subdirectory in which to save the file
+        suffix: New file extension for the local file
+    """
     models_dir = models_root / subdir
-    models_dir.mkdir(exist_ok=True)
+    models_dir.mkdir(parents=True, exist_ok=True)
+    parsed = urlparse(url)
+    rdf_path = Path(parsed.path).with_suffix(suffix)
+    rdf_name = rdf_path.with_suffix(".yaml").name
+    return models_dir / rdf_name
 
-    for model_url in rdf_urls:
-        parsed = urlparse(model_url)
-        rdf_path = Path(parsed.path)
-        rdf_name = rdf_path.with_suffix(".yaml").name
+def rdf_to_linkml(rdf_url: str, subdir: str, format: str) -> None:
+    """
+    Converts a remote RDF file to LinkML YAML
 
-        output_path = models_dir / rdf_name
-        schema = RdfsImportEngine().convert(model_url, format=format)
-        YAMLDumper().dump(schema, str(output_path))
+    Params:
+        rdf_url:  URL of the RDF file
+        subdir: subdirectory to save the YAML file
+        format: format of the RDF file (e.g. "xml", "ttl")
+    """
+    output_path = url_to_path(rdf_url, subdir)
+    schema = RdfsImportEngine().convert(rdf_url, format=format)
+    YAMLDumper().dump(schema, str(output_path))
+
+def validate_linkml(model_path: Path) -> None:
+    """
+    Validates a LinkML YAML file and raises an error if any validation problems are found
+    """
+    for problem in Linter().validate_schema(model_path):
+        if problem.level is RuleLevel.error:
+            raise ValueError(problem.message)
+
+def download_and_validate(rdf_url: str, subdir: str, format: str) -> Iterable[dict[str, Any]]:
+    """
+    Task generator that yields two tasks: one to convert an RDF file to LinkML YAML, and another to validate the YAML file
+    """
+    output_path = url_to_path(rdf_url, subdir)
+    yield {
+        "name": f"Convert {rdf_url}",
+        "actions": [
+            partial(
+                rdf_to_linkml,
+                rdf_url,
+                subdir=subdir,
+                format=format
+            )
+        ],
+        "targets": [output_path],
+    }
+    yield {
+        "name": f"Validate {rdf_url}",
+        "file_dep": [output_path],
+        "actions": [partial(validate_linkml, output_path)],
+    }
 
 
 def task_prof():
     """
     Converts prof ontology from RDF to LinkML YAML
     """
-    return {
-        "actions": [
-            partial(
-                rdf_to_linkml,
-                ["https://www.w3.org/TR/dx-prof/rdf/prof.ttl"],
-                subdir="prof",
-                format="ttl",
-            )
-        ],
-        "targets": ["models/prof/prof.yaml"],
-    }
-
+    yield from download_and_validate("https://www.w3.org/TR/dx-prof/rdf/prof.ttl", "prof", "ttl")
 
 def task_skos():
     """
     Converts SKOS Simple Knowledge Organization System from RDF to LinkML YAML
     """
-    return {
-        "actions": [
-            partial(
-                rdf_to_linkml,
-                ["https://www.w3.org/2004/02/skos/core.rdf"],
-                subdir="skos",
-                format="xml",
-            )
-        ],
-        "targets": ["models/skos/core.yaml"],
-    }
-
+    yield from download_and_validate("https://www.w3.org/2004/02/skos/core.rdf", "skos", "xml")
 
 def task_dc():
     """
     Converts Dublin Core from RDF to LinkML YAML
     """
-    return {
-        "actions": [
-            partial(
-                rdf_to_linkml,
-                [
-                    "https://www.dublincore.org/specifications/dublin-core/dcmi-terms/dublin_core_terms.ttl"
-                ],
-                subdir="dc",
-                format="ttl",
-            )
-        ],
-        "targets": ["models/dc/core.yaml"],
-    }
-
+    yield from download_and_validate("https://www.dublincore.org/specifications/dublin-core/dcmi-terms/dublin_core_terms.ttl", "dc", "ttl")
 
 def task_sdo():
     """
     Converts Schema.org from RDF to LinkML YAML
     """
-    return {
-        "actions": [
-            partial(
-                rdf_to_linkml,
-                ["https://schema.org/version/latest/schemaorg-current-https.ttl"],
-                subdir="sdo",
-                format="ttl",
-            )
-        ],
-        "targets": ["models/sdo/schemaorg-current-https.yaml"],
-    }
+    yield from download_and_validate("https://schema.org/version/latest/schemaorg-current-https.ttl", "sdo", "ttl")
 
 def task_pcdm():
     """
@@ -95,15 +99,6 @@ def task_pcdm():
     """
     # Using my fork of the PCDM ontology due to https://github.com/duraspace/pcdm/issues/80
     base_url = "https://raw.githubusercontent.com/multimeric/pcdm/refs/heads/fix-80/"
-    urls = ["models.rdf", "pcdm-ext/file-format-types.rdf", "pcdm-ext/rights.rdf", "pcdm-ext/use.rdf", "pcdm-ext/works.rdf"]
-    return {
-        "actions": [
-            partial(
-                rdf_to_linkml,
-                [urljoin(base_url, x) for x in urls],
-                subdir="pcdm",
-                format="xml"
-            )
-        ],
-        "targets": [str(models_root / path) for path in urls],
-    }
+    for url in ["models.rdf", "pcdm-ext/file-format-types.rdf", "pcdm-ext/rights.rdf", "pcdm-ext/use.rdf", "pcdm-ext/works.rdf"]:
+        full_url = urljoin(base_url, url)
+        yield from download_and_validate(full_url, "pcdm", "xml")
--- models.md
+++ models.md
@@ -2,8 +2,24 @@
 
 ## Dublin Core
 
-[dublin_core_terms.yaml](models/dc/dublin_core_terms.yaml){:download="dublin_core_terms.yaml"}
+- [dublin_core_terms.yaml](models/dc/dublin_core_terms.yaml)
 
 ## Portland Common Data Model
 
-[.yaml](models/dc/dublin_core_terms.yaml){:download="dublin_core_terms.yaml"}
+- [models.yaml](models/pcdm/models.yaml)
+- [rights.yaml](models/pcdm/rights.yaml)
+- [file-format-types.yaml](models/pcdm/file-format-types.yaml)
+- [use.yaml](models/pcdm/use.yaml)
+- [works.yaml](models/pcdm/models.yaml)
+
+## The Profiles Vocabulary
+
+- [prof.yaml](models/prof/prof.yaml)
+
+## Schema.org
+
+- [schemaorg-current-https.yaml](models/sdo/schemaorg-current-https.yaml)
+
+## Simple Knowledge Organization System
+
+- [core.yaml](models/skos/core.yaml)
