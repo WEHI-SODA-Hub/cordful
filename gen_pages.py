15ea5c9df738892500cba21f3c68b0ec9b1bcfdf
Michael Milton <michael.r.milton@gmail.com>
2025-01-24 15:02:27 +1100

    Separate bioschemas into two subheadings

--- gen_pages.py
+++ gen_pages.py
@@ -12,9 +12,17 @@ with mkdocs_gen_files.open("models.md", "w") as f:
         ("pcdm", "Portland Common Data Model (PCDM)"), 
         ("prof", "The Profiles Vocabulary (PROF)"),
         ("sdo", "Schema.org"),
-        ("bioschemas", "Bioschemas"),
     ]:
         f.write(f"## {vocab_name}\n")
         subdir = models / subdir_name
         for file in sorted(subdir.iterdir()):
             f.write(f"- [{file.stem}]({file.relative_to(models)})\n")
+
+    # Add bioschemas separately since it has a different structure
+    f.write(f"## Bioschemas\n")
+    f.write(f"### Types\n")
+    for file in sorted((models / "bioschemas" / "types").iterdir()):
+        f.write(f"- [{file.stem}]({file.relative_to(models)})\n")
+    f.write(f"### Profiles\n")
+    for file in sorted((models / "bioschemas" / "profiles").iterdir()):
+        f.write(f"- [{file.stem}]({file.relative_to(models)})\n")
