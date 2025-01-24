82eeec9566f997d11d74dc33a07388c3b4e6a850
Michael Milton <michael.r.milton@gmail.com>
2025-01-24 14:16:00 +1100

    Auto generate models.md

---
+++ gen_pages.py
@@ -0,0 +1,20 @@
+import mkdocs_gen_files
+from pathlib import Path
+
+models = Path(__file__).parent / "models"
+
+with mkdocs_gen_files.open("models.md", "w") as f:
+    f.write("# Models\n")
+
+    for subdir_name, vocab_name in [
+        ("dc", "Dublin Core (DC)"),
+        ("skos", "Simple Knowledge Organization System (SKOS)"),
+        ("pcdm", "Portland Common Data Model (PCDM)"), 
+        ("prof", "The Profiles Vocabulary (PROF)"),
+        ("sdo", "Schema.org"),
+        ("bioschemas", "Bioschemas"),
+    ]:
+        f.write(f"## {vocab_name}\n")
+        subdir = models / subdir_name
+        for file in subdir.iterdir():
+            f.write(f"- [{file.stem}]({file.relative_to(models)})\n")
